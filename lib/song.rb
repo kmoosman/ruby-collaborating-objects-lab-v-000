3406b6fcfb3c04a496e51c3f09b11acce62b80a9
Katie Moosman <moosman.katie@gmail.com>
2018-12-23 04:01:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,9 @@ class Song
   
   def self.new_by_filename(file_name)
     song_name = file_name.split(" - ")
-    song = self.new(song_name[1])
     self.artist.name = song_name[0]
+    song = self.new(song_name[1])
+    
   end
   
     
